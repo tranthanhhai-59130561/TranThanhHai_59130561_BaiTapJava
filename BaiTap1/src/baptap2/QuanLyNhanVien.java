700124e5c57c9d66dcea5c7cc878595c8feb62f6
Admin <Admin@User>
2020-04-12 18:19:03 +0700

    Fix_BaiTap2

--- BaiTap1/src/baptap2/QuanLyNhanVien.java
+++ BaiTap1/src/baptap2/QuanLyNhanVien.java
@@ -4,11 +4,25 @@
  * and open the template in the editor.
  */
 package baptap2;
-
+import baitap1.NhanVien;
+import java.util.ArrayList;
 /**
  *
  * @author Admin
  */
 public class QuanLyNhanVien {
-    
+    public class QuanLyNhanVien implements IQuanLy{
+    // danh sach nhan vien
+  ArrayList<NhanVien> dsNhanVien = new ArrayList<>();
+
+  // implement methods
+  
+  public void them(NhanVien nv) {
+    dsNhanVien.add(nv);
+  };
+  
+  
+  public void inDS() {
+    dsNhanVien.forEach(nv -> System.out.println(nv.getThongTin()));
+  };
 }
